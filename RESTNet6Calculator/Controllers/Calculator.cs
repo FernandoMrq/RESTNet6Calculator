ed86de2cb28e1c984c1c031ed5d224cfe42bef81
Fernando Marques de Oliveira <frndoliveira@gmail.com>
2022-08-07 20:39:00 -0300

    New endpoints

--- RESTNet6Calculator/Controllers/Calculator.cs
+++ RESTNet6Calculator/Controllers/Calculator.cs
@@ -15,7 +15,7 @@ namespace RESTNet6.Controllers
         }
 
         [HttpGet("sum/{firsNumber}/{secondNumber}")]
-        public IActionResult Get(string firsNumber, string secondNumber)
+        public IActionResult Sum(string firsNumber, string secondNumber)
         {
             if (IsNumeric(firsNumber)
                 && IsNumeric(secondNumber))
@@ -24,6 +24,81 @@ namespace RESTNet6.Controllers
                 return Ok(sum.ToString());
             }
             return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("multiply/{firsNumber}/{secondNumber}")]
+        public IActionResult Multiply(string firsNumber, string secondNumber)
+        {
+            if (IsNumeric(firsNumber)
+                && IsNumeric(secondNumber))
+            {
+                var multiply = ConvertToDecimal(firsNumber) * ConvertToDecimal(secondNumber);
+                return Ok(multiply.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("subtraction/{firsNumber}/{secondNumber}")]
+        public IActionResult Subtraction(string firsNumber, string secondNumber)
+        {
+            if (IsNumeric(firsNumber)
+                && IsNumeric(secondNumber))
+            {
+                var subtraction = ConvertToDecimal(firsNumber) - ConvertToDecimal(secondNumber);
+                return Ok(subtraction.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("division/{firsNumber}/{secondNumber}")]
+        public IActionResult Division(string firsNumber, string secondNumber)
+        {
+            if (IsNumeric(firsNumber)
+                && IsNumeric(secondNumber))
+            {
+                if (ConvertToDecimal(firsNumber) <= 0)
+                    return BadRequest("Invalid Input, first number is less or equal then 0.");
+
+                var division = ConvertToDecimal(firsNumber) / ConvertToDecimal(secondNumber);
+                return Ok(division.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("average/{firsNumber}/{secondNumber}")]
+        public IActionResult Average(string firsNumber, string secondNumber)
+        {
+            if (IsNumeric(firsNumber)
+                && IsNumeric(secondNumber))
+            {
+                if ((ConvertToDecimal(firsNumber) + ConvertToDecimal(secondNumber)) <= 0)
+                    return BadRequest("Invalid Input, sum is less or equal 0.");
+
+                var division = (ConvertToDecimal(firsNumber) + ConvertToDecimal(secondNumber)) / 2;
+                return Ok(division.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("squareRoot/{number}")]
+        public IActionResult SquareRoot(string number)
+        {
+            if (IsNumeric(number))
+            {
+                var division = Math.Sqrt(ConvertToDouble(number));
+                return Ok(division.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private double ConvertToDouble(string number)
+        {
+            double doubleValue;
+            if (double.TryParse(number, out doubleValue))
+            {
+                return doubleValue;
+            }
+            return 0;
         }
 
         private decimal ConvertToDecimal(string number)
